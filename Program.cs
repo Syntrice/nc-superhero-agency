0ac157d2b22ff03de78a4b3a9265362b76d188e0
Jonas Faria Costa <jonasfariacosta>
2024-11-15 10:45:32 +0000

    add until T2

--- Program.cs
+++ Program.cs
@@ -1,3 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
+List<string> list = new List<string>()
+{
+    "hello",
+    "world", 
+};
+
+Dictionary<int, string> dictionary = new Dictionary<int, string>()
+{
+    { 1, "a" },
+    { 2, "b" },
+};
+
+static T GetLastItem<T>(T[] list)
+{
+    return list[list.Length - 1];
+}
+
+int[] intArray = { 1, 5, 2, 8, 6 };
+Console.WriteLine(GetLastItem(intArray));
